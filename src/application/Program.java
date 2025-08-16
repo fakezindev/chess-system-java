76d37527d80a35783c564b1f0724778a91e52640
fakezindev <zetabr.bruno@gmail.com>
2025-08-15 22:20:49 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,9 +40,14 @@ public class Program {
 				}
 
 				if (chessMatch.getPromoted() != null) {
-					System.out.println("Ente piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
-					chessMatch.replacePromotedPiece(type);
+					System.out.print("Ente piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
+						chessMatch.replacePromotedPiece(type);
+					
 				}
 			}
 			catch (ChessException e) {
